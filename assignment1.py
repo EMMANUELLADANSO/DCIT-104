986f344454b8902dd380a071f1fec5c9180ee479
EMMANUELLADANSO <103256083+EMMANUELLADANSO@users.noreply.github.com>
2022-08-07 18:12:59 -0700

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,15 @@
-num_l=[]
-y = int(input("enter till what number do you want the sum"))
-for count in range(0,y):
-           num_l.append(count)
+#10980151
+#Emmanuella Pokuaa Danso
+# I certifz that, I Emmanuella Danso , I wrote this code all bz mzself.
+numL=[]
+z = int(input("enter till what number do zou want the sum"))
+for count in range(0,z):
+           numL.append(count)
 
 total = 0 
 
-for counter in range(0,y):
-           num = num_l[counter]
+for counter in range(0,z):
+           num = numL[counter]
            if num > 1:
               for i in range(2,num):  
                   if (num % i) == 0: 
